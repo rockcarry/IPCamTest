3ee7aee2dc5c113e395ef765cd1bf0c831430250
chenk <chenk@apical.com.cn>
2019-07-10 09:50:33 +0800

    first commit of IPCamTest.

---
+++ app/src/main/java/com/apical/ipcamtest/IpcamDetector.java
@@ -0,0 +1,163 @@
+package com.apical.ipcamtest;
+
+import android.os.Handler;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.ArrayList;
+
+class IpcamDev {
+    public long   tick;
+    public String ip;
+    public String uid;
+}
+
+public class IpcamDetector {
+    public static final int MSG_LIST_CHANGED = 1;
+
+    private final static boolean DEBUG          = false;
+    private final static int     PORT           = 8313;
+    private final static int     DEVLOST_TIMEOUT= 5000;
+    private DatagramSocket       mSocket        = null;
+    private Thread               mTxThread      = null;
+    private Thread               mRxThread      = null;
+    private boolean              mStopFlag      = false;
+    private DatagramPacket       mTxPacket      = null;
+    private DatagramPacket       mRxPacket      = null;
+    private byte[]               mRxData        = new byte[256];
+    private IpcamDev[]           mDevList       = new IpcamDev[256];
+    private ArrayList<String>    mIpcamList     = new ArrayList<String>();
+    private Handler              mHandler       = null;
+
+    public IpcamDetector(String ip, Handler handler) {
+        for (int i=0; i<mDevList.length; i++) mDevList[i] = new IpcamDev();
+        byte[] senddata = "uid?".getBytes();
+        try {
+            mSocket = new DatagramSocket();
+            mSocket.setSoTimeout(1000);
+            mTxPacket = new DatagramPacket(senddata, senddata.length, InetAddress.getByName(ip), PORT);
+            mRxPacket = new DatagramPacket(mRxData, mRxData.length);
+        } catch (Exception e) { e.printStackTrace(); }
+        mHandler = handler;
+    }
+
+    public void start() {
+        mStopFlag = false;
+        if (mTxThread == null) {
+            mTxThread = new Thread() {
+                @Override
+                public void run() {
+                    while (!mStopFlag) {
+//                      System.out.println("tx thread running...");
+                        try {
+                            mSocket.send(mTxPacket);
+                            Thread.sleep(1000);
+                        } catch (Exception e) { e.printStackTrace(); }
+                    }
+                    mTxThread = null;
+                }
+            };
+            mTxThread.start();
+        }
+        if (mRxThread == null) {
+            mRxThread = new Thread() {
+                @Override
+                public void run() {
+                    while (!mStopFlag) {
+//                      System.out.println("rx thread running...");
+                        boolean recvok = true;
+                        long    curtick= 0;
+                        boolean changed= false;
+
+                        try { mSocket.receive(mRxPacket); } catch (Exception e) { recvok = false; }
+                        curtick = System.currentTimeMillis();
+
+                        if (recvok) {
+                            String data = new String(mRxPacket.getData()).trim();
+                            String ip   = new String(mRxPacket.getAddress().getHostAddress());
+                            int    n    = Integer.parseInt(ip.substring(ip.lastIndexOf(".") + 1));
+//                          System.out.println("received data = " + data);
+
+                            mDevList[n].ip  = ip;
+                            if (data.startsWith("uid:")) {
+                                mDevList[n].uid = data.substring(4);
+                            }
+                            if (mDevList[n].tick == 0) {
+//                              System.out.println("device found: uid = " + mDevList[n].uid + ", ip = " + mDevList[n].ip);
+                                changed = true;
+                            }
+                            mDevList[n].tick= curtick;
+                        }
+
+                        for (int i=0; i<mDevList.length; i++) {
+                            if (mDevList[i].tick != 0 && curtick - mDevList[i].tick > DEVLOST_TIMEOUT) {
+//                              System.out.println("device lost: uid = " + mDevList[i].uid + ", ip = " + mDevList[i].ip);
+                                changed = true;
+                                mDevList[i].tick = 0;
+                                mDevList[i].ip   = "";
+                                mDevList[i].uid  = "";
+                            }
+                        }
+
+                        if (changed) {
+                            if (DEBUG) {
+                                boolean nodev = true;
+                                System.out.println("\ndevice list:");
+                                System.out.println("----------------------------------");
+                                for (int i=0; i<mDevList.length; i++) {
+                                    if (mDevList[i].tick != 0) {
+                                        System.out.println(mDevList[i].uid + " " + mDevList[i].ip);
+                                        nodev = false;
+                                    }
+                                }
+                                if (nodev) System.out.println("no device.");
+                                System.out.println("\n\n");
+                            }
+
+                            mIpcamList.clear();
+                            for (int i=0; i<mDevList.length; i++) {
+                                if (mDevList[i].tick != 0) {
+                                    mIpcamList.add(mDevList[i].uid + " " + mDevList[i].ip);
+                                }
+                            }
+                            if (mHandler != null) {
+                                mHandler.sendEmptyMessage(MSG_LIST_CHANGED);
+                            }
+                        }
+                    }
+                    mRxThread = null;
+                }
+            };
+            mRxThread.start();
+        }
+    }
+
+    public void stop() {
+        mStopFlag = true;
+        if (mTxThread != null) {
+            try { mTxThread.join(); } catch (Exception e) { e.printStackTrace(); }
+        }
+        if (mRxThread != null) {
+            try { mRxThread.join(); } catch (Exception e) { e.printStackTrace(); }
+        }
+    }
+
+    public ArrayList<String> getIpcamList() {
+        return mIpcamList;
+    }
+
+    /*
+    public static void main(String args[]) {
+        String ip = args.length > 0 ? new String(args[0]) : "255.255.255.255";
+        IpcamDetector detector = new IpcamDetector(ip);
+        detector.start();
+        while (true) {
+            int key = 0;
+            try { key =  System.in.read(); } catch (Exception e) { e.printStackTrace(); }
+            if (key == 'q' || key == 'Q') break;
+        }
+        detector.stop();
+    }*/
+}
+
---
+++ app/src/main/java/com/apical/ipcamtest/MainActivity.java
@@ -0,0 +1,101 @@
+package com.apical.ipcamtest;
+
+import android.content.ComponentName;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import com.apical.fanplayer.PlayerActivity;
+
+import java.util.*;
+
+public class MainActivity extends AppCompatActivity {
+    private IpcamDetector     mIpcamDetector;
+    private ListView          mIPCamListView;
+    private ArrayAdapter      mListAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mIpcamDetector = new IpcamDetector("255.255.255.255", mHandler);
+        mIPCamListView = (ListView)findViewById(R.id.lv_ipcams);
+        mListAdapter   = new ArrayAdapter(this, R.layout.item, mIpcamDetector.getIpcamList());
+        mIPCamListView.setAdapter(mListAdapter);
+        mIPCamListView.setOnItemClickListener(mOnItemClickListener);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mIpcamDetector.start();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mIpcamDetector.stop();
+    }
+
+    private AdapterView.OnItemClickListener mOnItemClickListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            final String[] opertions = new String[] { getString(R.string.admin_device), getString(R.string.watch_video) };
+            final String strItem = mIPCamListView.getItemAtPosition(position).toString();
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setItems(opertions, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int which) {
+                    switch (which) {
+                    case 0:
+                        try {
+                            String[] ss = strItem.split(" ");
+                            Uri uri = Uri.parse(String.format("http://%s", ss[1]));
+                            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                            startActivity(intent);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        break;
+                    case 1:
+                        try {
+                            String[] ss = strItem.split(" ");
+                            Uri uri = Uri.parse(String.format("rtsp://%s/video0", ss[1]));
+                            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                            intent.setComponent(new ComponentName("com.apical.ipcamtest", "com.apical.fanplayer.PlayerActivity"));
+                            startActivity(intent);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        break;
+                    }
+                }
+            });
+            builder.show();
+        }
+    };
+
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+            case IpcamDetector.MSG_LIST_CHANGED:
+                mListAdapter.notifyDataSetChanged();
+                break;
+            }
+        }
+    };
+}
